fd79ce60c017ead41282f177522ab563631165e6
Oliver-Noah <noahtoliver01@gmail.com>
2024-02-23 05:42:10 -0800

    completed scripts for backend

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# $1 will be the link to the https git clone link
+# $1 will be the https git clone link
 # $2 will be the name of the docker image, this will be a dropdown that the frontend selects
 
 #minikube should already be started, if not run "minikube start"
@@ -9,7 +9,6 @@
 set -eu
 
 #create temp workspace and clone project into it
-cd C:\Users\Owner\Documents
 mkdir tempWorkspace
 cd tempWorkspace
 git clone $1 #will clone input that was sent to backend
@@ -21,12 +20,8 @@ cd *
 docker build -t $2 . #will build a docker image named the second input
 
 #push the newly created local docker image directly to Minikube
-minikube image load $2
+minikube image load $2 --overwrite
 #can run "minikube image ls" to verify it loaded
 
 #run image in kube cluster
 kubectl run $2 --image=$2 --image-pull-policy=Never --restart=Never
-
-#cleanup the temp workspace, must be done from \Documents
-cd C:\Users\Owner\Documents
-rm -r tempWorkspace
