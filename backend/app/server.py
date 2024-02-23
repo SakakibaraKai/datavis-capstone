ada2187c67505010c57623e066b27192e9403664
ohjeo <ojm1818@gmail.com>
2024-02-23 04:21:57 -0800

    CORS

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, APIRouter
 from .routers import post, get
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origin to access to port : 8080
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(get.router)
 app.include_router(post.router)
