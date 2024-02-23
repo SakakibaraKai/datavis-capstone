22014de1e448b61534380bcf9c75d1ff787a86ca
ohjeo <ojm1818@gmail.com>
2024-02-23 04:18:11 -0800

    frontend connection to backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { People, PersonItem, PersonBasic, Root, ErrorPage } from './App'
 import './index.css'
 
 import FetchSearch from './pages/FetchSearch'
 
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
     //configuration
@@ -34,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router = {router} />
+        <QueryClientProvider client = {queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>
 )
