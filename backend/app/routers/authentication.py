da4f115159077418ea24cbf9597153331ac84327
ohjeo <ojm1818@gmail.com>
2024-03-15 15:28:50 -0700

    -

---
+++ backend/app/routers/authentication.py
@@ -0,0 +1,35 @@
+import jwt
+import bcrypt
+import os
+from dotenv import load_dotenv
+from jwt.exceptions import InvalidSignatureError
+
+load_dotenv()
+
+def validateToken(token : str):
+    token_secret : str = os.getenv('JWT_SECRET')
+    try:
+        payload = jwt.decode(token, token_secret, algorithms=["HS256"]) 
+        return False, payload
+    except InvalidSignatureError as e:
+        return True, "Invalid Token"
+
+    
+def createToken(id, name):
+    token_secret : str = os.getenv('JWT_SECRET')
+    token = jwt.encode({
+        "id" : id,
+        "name" : name,
+    }, "token_test", algorithm="HS256")
+
+    return token
+
+def validatePassword(password, hashedPassword, id, name):
+    if bcrypt.checkpw(password, hashedPassword):
+        token = createToken(id, name)
+        return False, token, 200
+    return True, "Unauthorized", 401
+
+def hashPassword(password):
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password, salt) 
--- backend/app/server.py
+++ backend/app/server.py
@@ -20,5 +20,5 @@ if ENV_FILE:
 app.secret_key = env.get("APP_SECRET_KEY")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
-    #app.run(debug=True)
+    #app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(debug=True)
