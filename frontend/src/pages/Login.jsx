a561e5287d34e93216404e449b8c02e8653d9d05
Jonathan Moreno <jonathan.moreno.farias02@gmail.com>
2024-05-13 23:34:49 -0700

    Change port

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        fetch("http://localhost:5000/login", {
+        fetch("http://localhost:8080/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
